71f83687b2905a8f41b54677a0d8c9a38de5dfef
Steven Doran <steven.doran@flatironschool.com>
2020-01-02 12:28:01 -0500

    Done.

--- index.js
+++ index.js
@@ -9,17 +9,17 @@
  for you to use if you need it!
  */
 
-let allWagesFor = function () {
-    let eligibleDates = this.timeInEvents.map(function (e) {
-        return e.date
-    })
+// let allWagesFor = function () {
+//     let eligibleDates = this.timeInEvents.map(function (e) {
+//         return e.date
+//     })
 
-    let payable = eligibleDates.reduce(function (memo, d) {
-        return memo + wagesEarnedOnDate.call(this, d)
-    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
+//     let payable = eligibleDates.reduce(function (memo, d) {
+//         return memo + wagesEarnedOnDate.call(this, d)
+//     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
-    return payable
-}
+//     return payable
+// }
 
 function createEmployeeRecord(emloyee) {
     return {
@@ -75,7 +75,7 @@ function wagesEarnedOnDate(date) {
 function allWagesFor() {
     return this.timeInEvents.reduce(function(totalWages, event){
         return totalWages + wagesEarnedOnDate.call(this, event.date)
-    }, 0)
+    }.bind(this), 0)
 }
 
 function calculatePayroll(records) {
